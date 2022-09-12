29329f6b137f4d69fb51cab849a64d5ba2554fe3
ImaDoofus <ImaDoofusyt@gmail.com>
2022-09-11 22:00:41 -0600

    finishing changes

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import { load } from 'cheerio';
 import Parser from 'rss-parser';
-import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import { parse } from 'yaml';
+import fs from 'fs';
 
-dotenv.config();
-
-let configuration = new Configuration({
-	apiKey: process.env.OPENAI_TOKEN
-});
-let openai = new OpenAIApi(configuration);
+let tokens = parse(fs.readFileSync('./tokens.yml', 'utf8'));
 
 let parser = new Parser();
 
@@ -18,47 +13,44 @@ async function alertHousingUpdate(post) {
 	let $ = load(rawData);
 	let postData = $('.bbWrapper')[0];
 
-	// get the update image src at top of post
+	$(postData).find('script').remove();
+
 	let postImage = $(postData).find('.bbImage');
 	let imageSource = $(postImage).attr('src');
+	
+	let summary = '**Summary**\n';
+	$(postData).find('span[style*=24px]').each((i, e) => summary += '• ' + $(e).text() + '\n');
 
-	let postText = $(postData).text();
-
-	// lol the ai is funny!
-	let completion = await openai.createCompletion({
-		model: 'text-davinci-002',
-		prompt: postText + '\nTldr;',
-	});
-
-	let summary = completion.data.choices[0].text;
-
-	let webhookURL = process.env.HOUSING_EDITOR_WEBHOOK;
-
-	await fetch(webhookURL, {
-		method: 'POST',
-		headers: {
-			'Content-type': 'application/json',
-		},
-		body: JSON.stringify({
-			username: 'Housing Update Pinger',
-			avatar_url: 'https://cdn.discordapp.com/avatars/384695177366732800/30040872b7d91a2f09e76803c89779c2.webp?size=64',
-			embeds: [{
-				title: post.title,
-				description: '**Summary:** ' + summary,
-				image: { url: imageSource },
-				thumbnail: { url: 'https://i.imgur.com/Y3VP3Wa.gif' },
-				footer: { text: 'by ImaDoofus :)' },
-				color: 2550450,
-			}]
+	for (let i = 0; i < tokens.webhooks.length; i++) {
+		let webhook = tokens.webhooks[i];
+		await fetch(webhook.url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify({
+				username: 'Housing Update Pinger',
+				avatar_url: 'https://cdn.discordapp.com/avatars/384695177366732800/30040872b7d91a2f09e76803c89779c2.webp?size=64',
+				content: webhook.message,
+				embeds: [{
+					title: post.title,
+					url: post.link,
+					description: summary,
+					image: { url: imageSource },
+					thumbnail: { url: 'https://i.imgur.com/Y3VP3Wa.gif' },
+					footer: { text: 'by ImaDoofus :)' },
+					color: 2550450,
+				}]
+			})
 		})
-	})
+	}
 
 	process.exit();
 }
 
 async function checkHousingUpdate() {
 	try {
-		let feed = await parser.parseURL('https://hypixel.net/forums/4/index.rss')
+		let feed = await parser.parseURL('https://hypixel.net/forums/4/index.rss');
 		if (!feed.items[0].title.includes('Housing')) return;
 		if (feed.items[0].creator !== 'ConnorLinfoot') return;
 		switch (feed.items[0].title) {
@@ -79,4 +71,4 @@ async function checkHousingUpdate() {
 checkHousingUpdate();
 setInterval(async () => {
 	await checkHousingUpdate();
-}, 1000 * 10)
+}, 1000 * 30)
